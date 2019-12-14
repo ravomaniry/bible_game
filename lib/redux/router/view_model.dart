c110ceb2e093af6ac9fc3f4682e2fb4de9adb6cd
Ravomaniry <arm@e2f.com>
2019-12-15 01:01:06 +0300

    router view model: routing to congrat

--- lib/redux/router/view_model.dart
+++ lib/redux/router/view_model.dart
@@ -6,16 +6,19 @@ import 'package:redux/redux.dart';
 class RouterViewModel {
   final Routes route;
   final bool gameIsResolved;
+  final bool activeGameIsCompleted;
 
   RouterViewModel({
     @required this.route,
     @required this.gameIsResolved,
+    @required this.activeGameIsCompleted,
   });
 
   static RouterViewModel converter(Store<AppState> store) {
     return RouterViewModel(
       route: store.state.route,
       gameIsResolved: store.state.game.isResolved,
+      activeGameIsCompleted: store.state.game.activeGameIsCompleted,
     );
   }
 }
