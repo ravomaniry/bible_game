293c83edde7e807df1a3d3f4fa0ada189f72c7e0
Ravomaniry <arm@e2f.com>
2019-12-19 23:28:48 +0300

    theme

--- lib/redux/themes/default_theme.dart
+++ lib/redux/themes/default_theme.dart
@@ -4,8 +4,9 @@ class DefaultTheme {
   final Color primary = const Color.fromARGB(255, 212, 38, 91);
   final Color primaryLight = const Color.fromARGB(255, 255, 113, 156);
   final Color primaryDark = const Color.fromARGB(255, 121, 0, 37);
-  final Color accentRight = const Color.fromARGB(255, 78, 255, 0);
+  final Color accentRight = const Color.fromARGB(255, 26, 186, 26);
   final Color accentRightDark = const Color.fromARGB(255, 29, 94, 0);
   final Color accentLeft = const Color.fromARGB(255, 0, 81, 255);
   final Color neutral = const Color.fromARGB(255, 255, 255, 255);
+  final String background = "assets/images/night.png";
 }
