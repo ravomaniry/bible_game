ad0af0216c3404bfa8727dc32d076915feebaf76
Ravomaniry <arm@e2f.com>
2019-12-23 20:25:59 +0300

    Lowered volume for in-game sfx

--- lib/sfx/sfx.dart
+++ lib/sfx/sfx.dart
@@ -7,24 +7,24 @@ class Sfx {
   final _bonus = "sfx/bonus.mp3";
 
   void playShortSuccess() {
-    _play(_shortSuccess);
+    _play(_shortSuccess, 0.5);
   }
 
   void playLongSuccess() {
-    _play(_longSuccess);
+    _play(_longSuccess, 1);
   }
 
   void playGreeting() {
-    _play(_greeting);
+    _play(_greeting, 1);
   }
 
   void playBonus() {
-    _play(_bonus);
+    _play(_bonus, 0.5);
   }
 
-  void _play(String path) async {
+  void _play(String path, double volume) async {
     try {
-      await AudioCache().play(path);
+      await AudioCache().play(path, volume: volume);
     } catch (e) {
       print("%%%%%%%%%% error in playShortSuccess %%%%%%%%%");
     }
