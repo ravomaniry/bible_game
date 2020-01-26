dc7557f81d53f6ffc8efa357eb516ed877a059fc
Ravomaniry <arm@e2f.com>
2020-01-26 21:38:50 +0300

    Scroll test draft

--- test/maze/scroller_test.dart
+++ test/maze/scroller_test.dart
@@ -1,7 +1,18 @@
+import 'package:bible_game/app/game/actions/actions.dart';
+import 'package:bible_game/app/router/actions.dart';
+import 'package:bible_game/app/router/routes.dart';
+import 'package:bible_game/games/maze/actions/actions.dart';
+import 'package:bible_game/games/maze/actions/board_utils.dart';
 import 'package:bible_game/games/maze/components/scroller.dart';
 import 'package:bible_game/games/maze/models/board.dart';
+import 'package:bible_game/games/maze/redux/state.dart';
+import 'package:bible_game/main.dart';
+import 'package:bible_game/models/bible_verse.dart';
+import 'package:bible_game/test_helpers/store.dart';
 import 'package:bible_game/utils/pair.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -74,5 +85,54 @@ void main() {
     expect(getOnScreenLimit(origin, board, containerSize), Pair(Size(2, 1), Size(5, 5)));
     containerSize = Size(1000, 1000);
     expect(getOnScreenLimit(origin, board, containerSize), Pair(Size(2, 1), Size(7, 5)));
+  });
+
+  testWidgets("Scroll and propose", (tester) async {
+    // This height output 300x460 (12.5 x 15)
+    tester.binding.window.physicalSizeTestValue = Size(300, 507);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    final double yOffset = 507.0 - 460;
+    final board = Board.create(20, 20); // 480, 480
+    board..set(2, 0, 0, 0)..set(2, 1, 0, 2)..set(2, 2, 0, 2);
+    final verse = BibleVerse.from(text: "Jesosy nitomany", bookId: 4, book: "Jaona", chapter: 11, verse: 33);
+    final store = newMockedStore();
+    await tester.pumpWidget(BibleGame(store));
+    await tester.pump(Duration(seconds: 1));
+    store.dispatch(GoToAction(Routes.maze));
+    store.dispatch(UpdateGameVerse(verse));
+    store.dispatch(UpdateMazeState(MazeState(
+      board: board,
+      wordsToFind: getWordsInScopeForMaze(verse),
+    )));
+    await tester.pump(Duration(seconds: 1));
+    // render and should display correctly
+    expect(find.byKey(Key("maze_board")), findsOneWidget);
+    addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+
+    /// SCROLL
+    // Tap on 10,10 and drag 20px to the left
+    final positionedFinder = find.byKey(Key("board_positioned"));
+    var gesture = await tester.startGesture(Offset(4, 4 + yOffset), pointer: 5);
+    // left -10 goes to (-10, 0)
+    await gesture.moveBy(Offset(-10, 0));
+    await tester.pump();
+    Positioned positioned = positionedFinder.evaluate().single.widget;
+    expect(positioned.top, 0);
+    expect(positioned.left, -10);
+    // left 20, up 20 goes to (0, 0)
+    await gesture.moveBy(Offset(20, 20));
+    await tester.pump();
+    positioned = positionedFinder.evaluate().single.widget;
+    expect(positioned.top, 0);
+    expect(positioned.left, 0);
+    // (0, 0) + (-100, -100)
+    await gesture.moveBy(Offset(-100, -100));
+    await tester.pump();
+    positioned = positionedFinder.evaluate().single.widget;
+    expect(positioned.top, -20);
+    expect(positioned.left, -100);
+
+    await gesture.up();
   });
 }
