6affc7c8242f961cb9ebc54640e0302bec87879e
Ravomaniry <arm@e2f.com>
2020-02-02 16:14:48 +0300

    Footer ui

---
+++ lib/games/maze/components/footer.dart
@@ -0,0 +1,93 @@
+import 'package:bible_game/app/theme/themes.dart';
+import 'package:bible_game/games/maze/models/board.dart';
+import 'package:bible_game/games/maze/models/coordinate.dart';
+import 'package:bible_game/games/maze/redux/view_model.dart';
+import 'package:bible_game/models/word.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+class Footer extends StatelessWidget {
+  final List<Coordinate> _selected;
+
+  Footer(this._selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector(
+      converter: MazeViewModel.converter,
+      builder: _builder,
+    );
+  }
+
+  Widget _builder(_, MazeViewModel viewModel) {
+    final theme = viewModel.theme;
+    final board = viewModel.state.board;
+    final wordsToFind = viewModel.state.wordsToFind;
+    return _FooterContainer(
+      theme: theme,
+      child: Text(
+        _getText(wordsToFind, board),
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        ),
+      ),
+    );
+  }
+
+  String _getText(List<Word> wordsToFind, Board board) {
+    var value = "";
+    if (_selected != null) {
+      for (final point in _selected) {
+        final cell = board.getAt(point.x, point.y).first;
+        if (cell.wordIndex >= 0) {
+          value += wordsToFind[cell.wordIndex].chars[cell.charIndex].comparisonValue.toUpperCase();
+        } else {
+          value += "-";
+        }
+      }
+    }
+    return value;
+  }
+}
+
+class _FooterContainer extends StatelessWidget {
+  final Widget child;
+  final AppColorTheme theme;
+
+  _FooterContainer({
+    @required this.child,
+    @required this.theme,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 2, bottom: 4, left: 20, right: 20),
+      child: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 100),
+          child: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              color: theme.primaryDark,
+              border: Border.all(color: theme.primaryLight, width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+              boxShadow: [
+                const BoxShadow(
+                  color: Color.fromARGB(180, 0, 0, 0),
+                  offset: Offset(2, 2),
+                  blurRadius: 2,
+                )
+              ],
+            ),
+            alignment: Alignment.center,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/games/maze/components/maze.dart
+++ lib/games/maze/components/maze.dart
@@ -1,18 +1,104 @@
 import 'package:bible_game/app/game/components/in_game_header.dart';
-import 'package:bible_game/games/maze/components/controller.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:bible_game/games/maze/components/footer.dart';
+import 'package:bible_game/games/maze/components/maze_board.dart';
+import 'package:bible_game/games/maze/components/path.dart';
+import 'package:bible_game/games/maze/components/scroller.dart';
+import 'package:bible_game/games/maze/components/tap_handler.dart';
+import 'package:bible_game/games/maze/models/board.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class Maze extends StatelessWidget {
+// The aim of this class is to hold states and delegate events handling to the
+// underlying handlers
+class Maze extends StatefulWidget {
+  @override
+  _MazeState createState() => _MazeState();
+}
+
+class _MazeState extends State<Maze> {
+  final _tapHandler = TapHandler();
+  final _scroller = Scroller();
+
+  void _reRender() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scroller.reRender = _reRender;
+    _tapHandler.reRender = _reRender;
+  }
+
+  void _onPointerDown(PointerDownEvent e, Board board) {
+    _tapHandler.onPointerDown(e, board);
+  }
+
+  void _onPointerMove(PointerMoveEvent e) {
+    final handled = _tapHandler.onPointerMove(e);
+    if (!handled) {
+      _scroller.onScroll(e);
+    }
+  }
+
+  void _onPointerUp(PointerUpEvent e) {
+    _tapHandler.onPointerUp(e);
+  }
+
+  void _adjustBoardSize(Board board) {
+    _scroller.adjustBoardSize(board);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: Key("gameScreen"),
+      backgroundColor: Color.fromARGB(255, 153, 208, 70),
       body: Column(
         children: [
           InGameHeader(),
-          MazeController(),
+          _buildBody(),
+          Footer(_tapHandler.selectedCells),
         ],
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Expanded(
+      child: Container(
+        child: LayoutBuilder(
+          builder: (_, constraints) {
+            _scroller.updateContainerSize(constraints);
+            return Stack(
+              overflow: Overflow.clip,
+              children: [
+                Positioned(
+                  top: _scroller.origin.height,
+                  left: _scroller.origin.width,
+                  key: Key("board_positioned"),
+                  child: Stack(
+                    children: [
+                      MazeBoard(
+                        onPointerDown: _onPointerDown,
+                        onPointerMove: _onPointerMove,
+                        screenLimit: _scroller.screenLimit,
+                        adjustBoardSize: _adjustBoardSize,
+                        onPointerUp: _onPointerUp,
+                      ),
+                      AbsorbPointer(
+                        child: MazePaths(
+                          start: _tapHandler.lineStart,
+                          end: _tapHandler.lineEnd,
+                          selected: _tapHandler.selectedCells,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
