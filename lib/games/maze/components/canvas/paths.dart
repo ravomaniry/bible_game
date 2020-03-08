15b7266beb4eb713245752292c442a9013be9333
Ravomaniry <arm@e2f.com>
2020-03-08 22:08:39 +0300

    maze revealed style

--- lib/games/maze/components/canvas/paths.dart
+++ lib/games/maze/components/canvas/paths.dart
@@ -8,15 +8,31 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-final _tmpLinePaint = Paint()
-  ..style = PaintingStyle.stroke
-  ..strokeWidth = 6
-  ..color = Colors.deepOrange.withAlpha(150);
+final _linePaintCache = Map<AppColorTheme, Paint>();
+final _checkpointPaintCache = Map<AppColorTheme, Paint>();
 
-final _tmpCirclePaint = Paint()
-  ..style = PaintingStyle.stroke
-  ..strokeWidth = 2
-  ..color = Colors.deepOrange;
+Paint _getLinePaint(AppColorTheme theme) {
+  var paint = _linePaintCache[theme];
+  if (paint == null) {
+    paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 12
+      ..color = theme.primary.withAlpha(180);
+    _linePaintCache[theme] = paint;
+  }
+  return paint;
+}
+
+Paint _getCheckpointPaint(AppColorTheme theme) {
+  var paint = _checkpointPaintCache[theme];
+  if (paint == null) {
+    paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = theme.primary;
+    _checkpointPaintCache[theme] = paint;
+  }
+  return paint;
+}
 
 class MazePaths extends StatelessWidget {
   @override
@@ -53,24 +69,27 @@ class _Painter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    final linePaint = _getLinePaint(_theme);
+    final rectPaint = _getCheckpointPaint(_theme);
+
     for (var pathIndex = 0; pathIndex < _paths.length; pathIndex++) {
       final path = _paths[pathIndex];
       for (var i = 0, max = path.length; i < max; i++) {
-        _drawCheckpoint(path[i], canvas);
+        _drawCheckpoint(path[i], canvas, rectPaint);
         if (i != max - 1) {
-          _drawLine(path[i], path[i + 1], canvas);
+          _drawLine(path[i], path[i + 1], canvas, linePaint);
         }
       }
     }
-    _drawCheckpoint(_end, canvas);
+    _drawCheckpoint(_end, canvas, rectPaint);
   }
 
-  void _drawCheckpoint(Coordinate coordinate, Canvas canvas) {
-    canvas.drawRRect(_getCheckpointRect(coordinate), _tmpCirclePaint);
+  void _drawCheckpoint(Coordinate coordinate, Canvas canvas, Paint paint) {
+    canvas.drawRRect(_getCheckpointRect(coordinate), paint);
   }
 
-  void _drawLine(Coordinate start, Coordinate end, Canvas canvas) {
-    canvas.drawLine(_centerInPx(start), _centerInPx(end), _tmpLinePaint);
+  void _drawLine(Coordinate start, Coordinate end, Canvas canvas, Paint paint) {
+    canvas.drawLine(_centerInPx(start), _centerInPx(end), paint);
   }
 
   @override
@@ -86,14 +105,14 @@ Offset _centerInPx(Coordinate point) {
   );
 }
 
-final _padding = 4;
-final _radius = Radius.circular(4);
+final _padding = 2;
+final _radius = Radius.circular(2);
 
 RRect _getCheckpointRect(Coordinate point) {
   final topLeft = Offset(cellSize * point.x, cellSize * point.y);
   return RRect.fromLTRBR(
-    topLeft.dx + _padding,
-    topLeft.dy + _padding,
+    topLeft.dx + 2 * _padding,
+    topLeft.dy + 2 * _padding,
     topLeft.dx + cellSize - _padding,
     topLeft.dy + cellSize - _padding,
     _radius,
