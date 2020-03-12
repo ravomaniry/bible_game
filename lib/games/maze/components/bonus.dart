2997086e14784df162f47f9822a62eae63bf1e3d
Ravomaniry <arm@e2f.com>
2020-03-12 09:51:27 +0300

    Maze bonus code improvements

--- lib/games/maze/components/bonus.dart
+++ lib/games/maze/components/bonus.dart
@@ -1,21 +1,26 @@
+import 'package:bible_game/app/app_state.dart';
 import 'package:bible_game/app/inventory/components/in_game_bonuses.dart';
-import 'package:bible_game/games/maze/redux/board_view_model.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+bool _converter(Store<AppState> store) {
+  return store.state.maze.board != null;
+}
 
 class MazeBonus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StoreConnector(
-      converter: BoardViewModel.converter,
+      converter: _converter,
       builder: _builder,
     );
   }
 
-  Widget _builder(BuildContext context, BoardViewModel viewModel) {
-    if (viewModel.state.board == null) {
-      return SizedBox.shrink();
+  Widget _builder(BuildContext context, bool isReady) {
+    if (isReady) {
+      return InGameBonuses();
     }
-    return InGameBonuses();
+    return SizedBox.shrink();
   }
 }
