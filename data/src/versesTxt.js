800bef576280b6bbd752eedbc02255204bd44eab
Ravomaniry <arm@e2f.com>
2020-03-26 20:31:03 +0300

    Verse lines extractor

---
+++ data/src/versesTxt.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function versesToTxt() {
+    const content = fs.readFileSync(path.join(__dirname, '../full/verses.json'), 'utf8');
+    const verses = JSON.parse(content);
+    const output = verses.map(({ book, chapter, verse, text }) =>
+        `${book} ${chapter} ${verse} ${text}`
+    ).join('\n');
+    fs.writeFileSync(path.join(__dirname, '../output/verses.txt'), output, 'utf8');
+}
+
+
+versesToTxt();
